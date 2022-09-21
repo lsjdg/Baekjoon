97d474ca2aa4cf9d856c3a9cc8bdb172954d9edc
이승준 <104449960+INHACSE-SeungjunLee@users.noreply.github.com>
2022-09-21 19:17:20 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int F(int n);
+
+int main() {
+	int n;
+	cin >> n;
+	cout << F(n);
+	return 0;
+}
+
+int F(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	else if (n == 1) {
+		return 1;
+	}
+	return F(n - 1) + F(n - 2);
+}
